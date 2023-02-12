15a9e3d23f7d074be3438cec0e092e53a2ddb04d
jordi-jaspers <34105935+Jordi-Jaspers@users.noreply.github.com>
2023-02-12 22:01:09 +0100

    ANI-46: creating a dockerfile for a CI/CD.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Loader from "@components/Loader";
 import RouteGuard from "@components/RouteGuard";
 import '@styles/globals.css'
+import {LOGGER} from "@util/Logger";
 import type {AppProps} from 'next/app'
 import React from "react";
 import {QueryClient, QueryClientProvider} from "react-query";
@@ -20,4 +21,14 @@ export default function App({Component, pageProps}: AppProps) {
             </Loader>
         </QueryClientProvider>
     )
+}
+
+export async function getServerSideProps() {
+    LOGGER.info("[PocketBase] Initializing PocketBase with url: " + process.env.NEXT_PUBLIC_POCKETBASE_URL);
+    LOGGER.info("[Consumet] Initializing Consumet with url: " + process.env.NEXT_PUBLIC_CONSUMET_BASE_URL);
+    LOGGER.info("[Aniflix] Logging is set to: " + process.env.NEXT_PUBLIC_LOGGING_LEVEL);
+    
+    return {
+        props: {}
+    }
 }
