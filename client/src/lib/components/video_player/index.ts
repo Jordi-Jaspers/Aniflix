14d8e0a20ebe7499dca41c285fc9e97d2095eab4
Jordi Jaspers <jordijaspers@gmail.com>
2024-05-16 12:46:47 +0200

    ANI-71: Adding previous episode button to mobile view.

--- client/src/lib/components/video_player/index.ts
+++ client/src/lib/components/video_player/index.ts
@@ -7,6 +7,7 @@ import Fullscreen from './fullscreen-button.svelte';
 import PictureInPicture from './pip-button.svelte';
 import Resolution from './resolution-button.svelte';
 import NextEpisode from './next-episode-button.svelte';
+import PreviousEpisode from './previous-episode-button.svelte';
 import DefaultPlayer from './default-player.svelte';
 import IOSPlayer from './ios-player.svelte';
 
@@ -20,6 +21,7 @@ export {
 	PictureInPicture,
 	Resolution,
 	NextEpisode,
+	PreviousEpisode,
 	DefaultPlayer,
 	IOSPlayer
 };
---
+++ client/src/lib/components/video_player/previous-episode-button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {goto} from '$app/navigation';
+    import {SkipBack} from 'lucide-svelte';
+
+    export let episode: EpisodeResponse;
+
+    function hasPrevEpisode(): boolean {
+        return episode.episodeNumber > 1;
+    }
+</script>
+
+<button
+        aria-label="Next Episode"
+        class="h-full w-full"
+        disabled={!hasPrevEpisode()}
+        on:click={() => goto('/watch/' + episode.anilistId + '/episode/' + (episode.episodeNumber - 1))}>
+    <SkipBack class="aspect-square h-full w-auto hover:scale-125"/>
+</button>
