67f3aca954f011788ac9ccc8dc78595c1e670800
Jordi Jaspers <jordijaspers@gmail.com>
2024-05-30 18:22:50 +0200

    Adding tracing to the application. USer cleanup.

--- server/src/main/java/org/jordijaspers/aniflix/api/user/repository/UserRepository.java
+++ server/src/main/java/org/jordijaspers/aniflix/api/user/repository/UserRepository.java
@@ -2,11 +2,13 @@ package org.jordijaspers.aniflix.api.user.repository;
 
 import org.jordijaspers.aniflix.api.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 /**
@@ -24,4 +26,13 @@ public interface UserRepository extends JpaRepository<User, Integer> {
      */
     @Query("FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     Optional<User> findByEmail(@NonNull String email);
+
+
+    /**
+     * Delete unvalidated accounts that are older than 1 month.
+     */
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM User u WHERE u.validated = false AND u.created <= :limit")
+    void deleteUnvalidatedAccounts(@NonNull LocalDateTime limit);
 }
--- server/src/main/java/org/jordijaspers/aniflix/email/service/sender/DevelopmentEmailService.java
+++ server/src/main/java/org/jordijaspers/aniflix/email/service/sender/DevelopmentEmailService.java
@@ -6,10 +6,14 @@ import org.jordijaspers.aniflix.api.token.model.Token;
 import org.jordijaspers.aniflix.api.token.model.TokenType;
 import org.jordijaspers.aniflix.api.token.service.TokenService;
 import org.jordijaspers.aniflix.email.model.MailMessage;
+import org.jordijaspers.aniflix.email.service.message.MailMessageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * A service to send emails in a development environment.
@@ -23,13 +27,21 @@ public class DevelopmentEmailService implements EmailService {
 
     private final TokenService tokenService;
 
+    private final MailMessageFactory mailMessageFactory;
+
     /**
      * {@inheritDoc}
      */
     @Override
     public void sendUserValidationEmail(final User recipient) {
+        final Map<String, Object> variables = new ConcurrentHashMap<>();
+
         final Token token = tokenService.generateToken(recipient, TokenType.USER_VALIDATION_TOKEN);
-        LOGGER.info("Sending validation code '{}' to user with email address '{}'", token.getValue(), recipient.getEmail());
+        LOGGER.info("Sending user validation email with token '{}' to user '{}'.", token.getValue(), recipient.getEmail());
+        variables.put("token", token.getValue());
+
+        final MailMessage message = mailMessageFactory.createUserValidationMessage(variables);
+        sendEmail(recipient, message);
     }
 
     /**
@@ -37,8 +49,14 @@ public class DevelopmentEmailService implements EmailService {
      */
     @Override
     public void sendPasswordResetEmail(final User recipient) {
+        final Map<String, Object> variables = new ConcurrentHashMap<>();
+
         final Token resetToken = tokenService.generateToken(recipient, TokenType.RESET_PASSWORD_TOKEN);
-        LOGGER.info("Sending password reset token '{}' to user with email address '{}'", resetToken.getValue(), recipient.getEmail());
+        LOGGER.info("Sending password reset with token '{}' to user '{}'.", resetToken.getValue(), recipient.getEmail());
+        variables.put("token", resetToken.getValue());
+
+        final MailMessage message = mailMessageFactory.createPasswordResetMessage(variables);
+        sendEmail(recipient, message);
     }
 
     /**
