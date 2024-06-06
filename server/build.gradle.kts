b8c80637693df7f36b58fc9d3e2bf83b2bfb5ae6
Jordi Jaspers <jordijaspers@gmail.com>
2024-06-06 21:12:44 +0200

    Release version 0.2.4

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -9,7 +9,7 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 
 // Both "group" and "version" are default Gradle properties, so they need to be set here
 group = "org.jordijaspers"
-version = "0.3.0-SNAPSHOT"
+version = "0.2.4"
 
 // Other, non-default Gradle, properties need to be defined here
 val author = "Jordi Jaspers"
